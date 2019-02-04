3ff674ce214e718863d16f68296011ff12143889
jerhodkyan <jerhodkyan@gmail.com>
2019-02-04 15:00:27 +0800

    Finished coding/ Final Output

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
 <body>
+
 <?php
-    
-$age = array("Peter" =>"35", "Ben" =>"37", "Joe" =>"43");
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+
+foreach($age as $x=>$x_value)
+  {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+  }
 ?>
-    
+
 </body>
 </html>
